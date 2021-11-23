f061e0de8b31da6b36052c78a92c4b1ee3c31a2f
v-hakita@expediagroup.com <v-hakita@expediagroup.com>
2021-11-22 20:07:26 -0600

    intro Js

---
+++ 1_JavaScript/0_Intro_JavaScript/app.js
@@ -0,0 +1,41 @@
+// Script = Instrucciones = Programa
+// Este es un comentario en JavaScript
+
+// Mi primer línea de código en Js
+
+// console.log nos permite enviar mensajes a la consola
+
+// Salidas a la consola ("Texto");
+console.log("Hola mundo!!");
+console.log(":D");
+console.log("Hoy es un día muy importante");
+
+// Salidas a la consola (Números);
+console.log(10);
+console.log(-10);
+console.log(10/5);
+console.log(10 + 30);
+
+// Salidas a la consola (Booleanos: verdadero o falso);
+console.log(true);
+console.log(false);
+console.log(5>2);
+console.log(5>20);
+
+// Salidas al documento o al explorador
+document.write("Hooola");
+document.write(":D");
+document.write("Jjeje");
+
+
+// Salidas como alertas
+alert("Soy una alerta");
+alert("Soy otra alerta");
+alert("Soy otra alerta, jejeje");
+
+
+// entrada a través de un prompt
+prompt("Escribe tu nombre");
+
+// Palabra reservada: es una palabra propia del lenguaje.
+// JavaScript ejecuta el código conforme lo encuentra es decir de izq->der arr->aba
