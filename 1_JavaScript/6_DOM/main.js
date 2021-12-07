0e3aecbbe055997d2761c4d1586e14ff2c79c5c8
v-hakita@expediagroup.com <v-hakita@expediagroup.com>
2021-12-06 20:15:01 -0600

    dom + js

--- 1_JavaScript/6_DOM/main.js
+++ 1_JavaScript/6_DOM/main.js
@@ -15,3 +15,18 @@ function elevarAlCuadrado(num) {
 function cambiarTamanio () {
     console.log('Creciendo...');
 }
+
+// Modificar estilos
+// Acceder a elementos a través de un id
+function cambiarFondo () {
+    // console.log(document.getElementById("body"));
+
+    // Forma 1
+    document.getElementById("body").style.backgroundColor = "green";
+    document.getElementById("body").style.color = "white";
+    document.getElementById("body").style.fontFamily = "Arial"
+    document.getElementById("body").style.padding = "30px"
+
+    // Forma 2
+    
+}
