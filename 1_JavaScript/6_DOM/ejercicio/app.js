eb96acc75efe8532f5d988ccca8f6d61085c4533
v-hakita@expediagroup.com <v-hakita@expediagroup.com>
2021-12-06 21:50:24 -0600

    DOM

---
+++ 1_JavaScript/6_DOM/ejercicio/app.js
@@ -0,0 +1,19 @@
+let indice = 0;
+
+function cambiarFondo () {
+    let colores = ["red", "blue", "yellow"];
+    document.getElementById("body").style.backgroundColor = colores[indice];
+    indice++;
+}
+
+
+function crecerImagen () {
+    let altura = document.getElementById("imagen").height;
+    altura += altura +50;
+    document.getElementById("imagen").height = altura;
+}
+
+function saludar () {
+    let nombre = prompt("Escribe tu nombre");
+    document.getElementById("titulo").innerText = "Hola " + nombre;
+}
