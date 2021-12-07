7c74dd95723960d571051497c763cbc818dc1cf0
v-hakita@expediagroup.com <v-hakita@expediagroup.com>
2021-12-06 19:35:42 -0600

    dom + js

---
+++ 1_JavaScript/6_DOM/main.js
@@ -0,0 +1,17 @@
+function saludarUsuario () {
+    console.log('bienvenido usuario!');
+}
+
+function mostrarNumeros () {
+    for(let i = 0; i <= 10; i++) {
+        console.log(i);
+    }
+}
+
+function elevarAlCuadrado(num) {
+    console.log(num**2);
+}
+
+function cambiarTamanio () {
+    console.log('Creciendo...');
+}
