d02deca8df39c8b8e4395009f0497f2dadfe9f14
v-hakita@expediagroup.com <v-hakita@expediagroup.com>
2021-12-07 20:30:08 -0600

    práctica

--- 1_JavaScript/6_DOM/nodos/main.js
+++ 1_JavaScript/6_DOM/nodos/main.js
@@ -18,8 +18,57 @@ parrafos[0].style.backgroundColor = 'red';
 for(let i = 0; i < parrafos.length; i++) {
     parrafos[i].style.backgroundColor = 'red';
 }
+
+parrafos[2].innerText = 'lo que sea';
+
 // parrafos es una colección de nodos -> NodeList
 // al ser una NodeList tenemos acceso a la propiedad length
 // parrafos NO es un array
 // al NO ser un array no podemos usar todos los métodos de los arrays
+
+
+// Obtener elementos por el nombre de clases
+
+let botonesCards = document.getElementsByClassName("button-card");
+console.log(botonesCards);
+
+for(let i = 0; i < botonesCards.length; i++) {
+    botonesCards[i].style.backgroundColor = 'blue';
+}
+
+// function deshabilitar () {
+//     let botonesCards = document.getElementsByClassName("button-card");
+//     for(let i = 0; i < botonesCards.length; i++) {
+//         botonesCards[i].disabled = true;
+//         botonesCards[i].style.backgroundColor = "gray";
+//     }
+// }
+
+function habilitar (botonHabilitado) {
+    let botonesCards = document.getElementsByClassName("button-card");
+    for(let i = 0; i < botonesCards.length; i++) {
+        botonesCards[i].disabled = botonHabilitado;
+        if(botonHabilitado) {
+            botonesCards[i].style.backgroundColor = "gray";
+        } else {
+            botonesCards[i].style.backgroundColor = "blue";
+            botonesCards[i].style.color = "white";
+        }
+    }
+}
+
+
+function cambiarEstilos() {
+    // Añadir una clase que ya contiene los estilos
+    document.getElementById("s2").classList.add("subtitulo");
+
+    // Añadir manualmente los estilos
+    // document.getElementById("s2").style.backgroundColor = "hotpink";
+    // document.getElementById("s2").style.color = "indigo";
+
+}
+
+function ocultarElemento () {
+    document.getElementById("s3").style.display = "none";
+}
 
